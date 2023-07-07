8e5090a7675736d0b952f32053a2dd70438a6ed9
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-07 22:40:36 +0200

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -32,3 +32,4 @@ def decrypt_file(file_path):
     print(f"Decryption complete. Decrypted file saved as {decrypted_file_path}")
 
 def main():
+    action = input("Enter 'encrypt' or 'decrypt' to choose an action: ")
