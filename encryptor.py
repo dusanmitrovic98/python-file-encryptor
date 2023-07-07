8263a442c9683a34f84cb2300638ec9707cf6a79
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-07 22:34:41 +0200

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -13,3 +13,4 @@ def encrypt_file(file_path):
     file_name_without_extension, file_extension = os.path.splitext(file_name)
     encrypted_file_path = f"{file_name_without_extension}-encrypted{file_extension}"
     with open(encrypted_file_path, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted_content)
